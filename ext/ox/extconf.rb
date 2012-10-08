da81dc74331e84827e8e4e8a26756801548654c8
Peter Ohler <peter@ohler.com>
2012-10-07 22:41:08 -0700

    added platform info to extconf.rb

--- ext/ox/extconf.rb
+++ ext/ox/extconf.rb
@@ -8,7 +8,7 @@ type = parts[0]
 type = 'ree' if 'ruby' == type && RUBY_DESCRIPTION.include?('Ruby Enterprise Edition')
 platform = parts[5][1...-1]
 version = RUBY_VERSION.split('.')
-puts ">>>>> Creating Makefile for #{type} version #{RUBY_VERSION} <<<<<"
+puts ">>>>> Creating Makefile for #{type} version #{RUBY_VERSION} on #{platform} <<<<<"
 
 dflags = {
   'RUBY_TYPE' => type,
