a87b7c1fb395483a75528cddcf55da827e340bd7
Peter Ohler <peter@ohler.com>
2017-09-21 20:31:23 -0700

    reverting changes

--- ext/ox/sax.c
+++ ext/ox/sax.c
@@ -175,7 +175,12 @@ sax_drive_init(SaxDrive dr, VALUE handler, VALUE io, SaxOptions options) {
     dr->buf.dr = dr;
     stack_init(&dr->stack);
     dr->handler = handler;
-    dr->value_obj = Data_Wrap_Struct(ox_sax_value_class, dr, 0, 0);
+    //dr->value_obj = Data_Wrap_Struct(ox_sax_value_class, dr, 0, 0);
+#if HAS_DATA_OBJECT_WRAP
+    dr->value_obj = rb_data_object_wrap(ox_sax_value_class, dr, 0, 0);
+#else
+    dr->value_obj = rb_data_object_alloc(ox_sax_value_class, dr, 0, 0);
+#endif
     rb_gc_register_address(&dr->value_obj);
     dr->options = *options;
     dr->err = 0;
