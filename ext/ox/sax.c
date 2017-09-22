0b50eb149f1903325ba22a0f4eb9624050c396fa
Peter Ohler <peter@ohler.com>
2017-09-21 20:40:16 -0700

    cleaned up commente dout code

--- ext/ox/sax.c
+++ ext/ox/sax.c
@@ -176,13 +176,6 @@ sax_drive_init(SaxDrive dr, VALUE handler, VALUE io, SaxOptions options) {
     stack_init(&dr->stack);
     dr->handler = handler;
     dr->value_obj = Data_Wrap_Struct(ox_sax_value_class, 0, 0, dr);
-#if 0
-#if HAS_DATA_OBJECT_WRAP
-    dr->value_obj = rb_data_object_wrap(ox_sax_value_class, dr, 0, 0);
-#else
-    dr->value_obj = rb_data_object_alloc(ox_sax_value_class, dr, 0, 0);
-#endif
-#endif
     rb_gc_register_address(&dr->value_obj);
     dr->options = *options;
     dr->err = 0;
